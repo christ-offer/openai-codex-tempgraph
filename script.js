b9f3ef5b623bda5549c819fee63cd0fbf1d09a45
Christoffer Holst <cholst@gmail.com>
2022-09-09 01:04:53 +0200

    added some more stuff, windspeed and min temp

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function getCoordinates(cityname) {
 }
 
 function getTemperature(lat, lon) {
-    const url = `https://archive-api.open-meteo.com/v1/era5?latitude=${lat}&longitude=${lon}&start_date=1960-01-01&end_date=2021-12-31&daily=temperature_2m_max,precipitation_sum&timezone=Europe%2FBerlin`;
+    const url = `https://archive-api.open-meteo.com/v1/era5?latitude=${lat}&longitude=${lon}&start_date=1960-01-01&end_date=2021-12-31&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,windspeed_10m_max&timezone=Europe%2FBerlin`;
     fetch(url)
         .then(response => response.json())
         .then(data => {
@@ -29,15 +29,21 @@ function getTemperature(lat, lon) {
             for (let i = 0; i < data.daily.time.length; i++) {
             const date = data.daily.time[i];
             const temperature = data.daily.temperature_2m_max[i];
+            const temperatureMin = data.daily.temperature_2m_min[i]
             const precipitation = data.daily.precipitation_sum[i];
+            const windspeed = data.daily.windspeed_10m_max[i];
             historical[date] = {
                 temperature,
-                precipitation
+                precipitation,
+                windspeed,
+                temperatureMin
             };
             }
             const labels = Object.keys(historical);
             const temperatures = Object.values(historical).map(item => item.temperature);
             const precipitations = Object.values(historical).map(item => item.precipitation);
+            const temperaturesMin = Object.values(historical).map(item => item.temperatureMin);
+            const windspeeds = Object.values(historical).map(item => item.windspeed);
             if (chart) {
                 chart.destroy();
             }
@@ -54,10 +60,24 @@ function getTemperature(lat, lon) {
                         borderWidth: 1
                     },
                     {
+                        label: 'Temperature Min',
+                        data: temperaturesMin,
+                        backgroundColor: 'rgba(0, 99, 132, 0.2)',
+                        borderColor: 'rgba(0, 99, 132, 1)',
+                        borderWidth: 1
+                    },
+                    {
                         label: 'Precipitation',
                         data: precipitations,
                         backgroundColor: 'rgba(122, 99, 132, 0.2)',
                         borderColor: 'rgba(122, 99, 132, 1)',
+                        borderWidth: 1
+                    },
+                    {
+                        label: 'Wind Speed',
+                        data: windspeeds,
+                        backgroundColor: 'rgba(35, 99, 132, 0.2)',
+                        borderColor: 'rgba(35, 99, 132, 1)',
                         borderWidth: 1
                     }
                     ]
