d15e7dc6b20bb7e305ca5812329396f448c40a54
Christoffer Holst <cholst@gmail.com>
2022-09-09 00:40:33 +0200

    manually added inn data for precipitation_sum also

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function getCoordinates(cityname) {
 }
 
 function getTemperature(lat, lon) {
-    const url = `https://archive-api.open-meteo.com/v1/era5?latitude=${lat}&longitude=${lon}&start_date=1960-01-01&end_date=2021-12-31&daily=temperature_2m_max&timezone=Europe%2FBerlin`;
+    const url = `https://archive-api.open-meteo.com/v1/era5?latitude=${lat}&longitude=${lon}&start_date=1960-01-01&end_date=2021-12-31&daily=temperature_2m_max,precipitation_sum&timezone=Europe%2FBerlin`;
     fetch(url)
         .then(response => response.json())
         .then(data => {
@@ -29,12 +29,15 @@ function getTemperature(lat, lon) {
             for (let i = 0; i < data.daily.time.length; i++) {
             const date = data.daily.time[i];
             const temperature = data.daily.temperature_2m_max[i];
+            const precipitation = data.daily.precipitation_sum[i];
             historical[date] = {
-                temperature, 
+                temperature,
+                precipitation
             };
             }
             const labels = Object.keys(historical);
             const temperatures = Object.values(historical).map(item => item.temperature);
+            const precipitations = Object.values(historical).map(item => item.precipitation);
             if (chart) {
                 chart.destroy();
             }
@@ -42,13 +45,22 @@ function getTemperature(lat, lon) {
                 type: 'line',
                 data: {
                     labels,
-                    datasets: [{
+                    datasets: [
+                    {
                         label: 'Temperature',
                         data: temperatures,
                         backgroundColor: 'rgba(255, 99, 132, 0.2)',
                         borderColor: 'rgba(255, 99, 132, 1)',
                         borderWidth: 1
-                    }]
+                    },
+                    {
+                        label: 'Precipitation',
+                        data: precipitations,
+                        backgroundColor: 'rgba(122, 99, 132, 0.2)',
+                        borderColor: 'rgba(122, 99, 132, 1)',
+                        borderWidth: 1
+                    }
+                    ]
                 },
                 options: {
                     scales: {
